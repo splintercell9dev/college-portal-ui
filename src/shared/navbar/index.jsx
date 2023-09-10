df1a1b8b14a41422418b1034310c7d9dfbd3d1d1
splintercell9dev <splintercell9dev@gmail.com>
2023-09-10 13:47:52 +0530

    Navbar component now handles the token expiration if refresh token expires to logout user..

--- src/shared/navbar/index.jsx
+++ src/shared/navbar/index.jsx
@@ -1,38 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../../store/user";
 import server from "../../utils/axios";
+import { getRefreshToken } from "../../utils/functions";
 
 
 function Navbar(){
-    const { user, refreshToken} = useSelector(state => state.user) ;
+    const { details } = useSelector(state => state.user) ;
     const navigate = useNavigate() ;
     const dispatch = useDispatch() ;
 
     useEffect(() => {
-        if (!user){
+        console.log('from use effect navbar', details.username)
+        if (!details){
             navigate('/', {
                 replace: true
             }) ;
         }
-    }, [user]) ;
+    }, [details]) ;
 
     const logout = () => {
+        console.log(getRefreshToken()) ;
         server.delete('logout', {
-            refreshToken
+            data: {
+                refreshToken: getRefreshToken()
+            }
         }).then( data => {
             console.log(data) ;
             dispatch(logoutUser()) ;
+            navigate('/', { replace: true }) ;
         }).catch(err=> {
             console.error(err) ;
+            if (err.response.data?.error.default === 'No token provided.'){
+                console.error('ran code')
+                navigate('/', { replace: true })
+            }
         })
     }
     return (
         <div className="navbar bg-light px-3 shadow-sm">
-            <span className="navbar-brand">College Portal</span>
+            <span className="navbar-brand">Dashboard</span>
             <div className="flex-grow-1 flex-shrink-1"></div>
-            <span className="me-3" style={{fontSize: '12px'}}>Welcome, { user?.username }</span>
+            <span className="me-3" style={{fontSize: '12px'}}>Welcome, { details.username || 'User' }</span>
             <button className="btn btn-outline-danger" onClick={logout}>Logout</button>
         </div>
     )
