f1ffa008c4fa54a4328acae4b584eb86fead5de6
splintercell9dev <splintercell9dev@gmail.com>
2023-09-10 13:45:57 +0530

    Added auto refresh access token and some UI changes

--- src/components/student/index.jsx
+++ src/components/student/index.jsx
@@ -1,149 +1,214 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux" ;
 import Navbar from '../../shared/navbar/index' ;
-import { useEffect, useState } from 'react';
-import server from '../../utils/axios';
-import { changeAccessToken } from '../../store/user' ;
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useRef, useState } from "react";
+import { getAccessToken, hasDetails, refreshAccessToken } from "../../utils/functions";
+import axios from "axios";
+import { logoutUser } from "../../store/user";
+import { useNavigate } from "react-router-dom";
+import server from "../../utils/axios";
 
 function Student(){
-    const { user, accessToken, refreshToken } = useSelector(state => state.user) ;
-    const navigate = useNavigate() ;
-    const [userData, setUserData] = useState({
-        name: null,
-        email: null,
-        contact: null,
-        address: null
-    }) ;
-    const [updated, setUpdated] = useState(false) ;
+    const [show, setShow] = useState(true) ;
+    const [success, setSuccess] = useState(false) ;
+    const detailsSource = axios.CancelToken.source() ;
+    const fileRef = useRef() ;
     const dispatch = useDispatch() ;
-    const refreshAccessToken = async () => {
-        try{
-            const { data } = await server.post('refresh', {
-                refreshToken
-            }) ;
+    const navigate = useNavigate() ;
+    const [busy, setBusy] = useState(false) ;
+    const [data, setData] = useState({
+        fullName: '',
+        email: '',
+        contact: '',
+        address: ''
+    })
+    const [formErrors, setFormErrors] = useState({
+        fullName: '',
+        email: '',
+        contact: '',
+        address: '',
+        file: '',
+        default: ''
+    }) ;
 
-            if (data.data.accessToken){
-                dispatch(changeAccessToken(data.data?.accessToken)) ;   
-            }
+    const logout = () => {
+        dispatch(logoutUser()) ;
+        navigate('/', { replace: true }) ;
+    } ;
 
-        }catch (err){
-            console.log(err)
-        }
-    }
-
-    const change = (ev) => {
+    const inputChange = (ev) => {
         const {name, value} = ev.target ;
-        setUserData({
-            ...userData,
+        setData({
+            ...data,
             [name]: value
         }) ;
+    } ;
+
+    const loadDetails = async () => {
+        const [data, error] = await hasDetails() ;
+        setShow(false) ;
+        if (!error && data === null){
+            setSuccess(false) ;
+        }
+        else if (!error && data !== null){
+            setData({
+                fullName: data.fullName,
+                email: data.email,
+                contact: data.contact,
+                address: data.address
+            }) ;
+            setSuccess(true) ;
+        }
     }
 
     const submit = () => {
-        const formData = new FormData() ;
-        
-        for(let key of Object.keys(userData)){
-            console.log('form value', userData[key])
-            formData.set(`${key}`, userData[key]) ;
+        setBusy(true) ;
+        const f = new FormData() ;
+
+        for(let [key, value] of Object.entries(data)){
+            f.set(key, value) ;
         }
 
-        formData.set("file", document.querySelector('input[type="file"]').files[0]) ;
-        console.log(formData)
+        f.set('file', fileRef.current.files[0]) ;
 
-        server.post('student/details', formData, {
+
+        server.post('student/details', f, {
             headers: {
                 Accept: 'application/json',
-                "Content-Type": 'multipart/form-data'
+                "Content-Type": 'mutipart/form-data',
+                Authorization: `Bearer ${getAccessToken()}`
             }
-        }).then( result => {
-            console.log(result) ;
-
+        }).then(res => {
+            console.log(res.data) ;
+            if (res.data?.success){
+                setSuccess(true) ;
+            }
         }).catch(err => {
-            if (err.response?.data.error === 'jwt expired'){
-                refreshAccessToken() ;
+            console.error(err) ;
+            const error = err?.response?.data?.error ;
+            if (error){
+                setFormErrors({
+                    fullName: error.fullName || '',
+                    email: error.email || '',
+                    contact: error.contact || '',
+                    address: error.address || '',
+                    file: error.file || '',
+                    default: error.default || ''
+                }) ;
             }
+        }).finally(() => {
+            setBusy(false) ;
         }) ;
     }
 
     useEffect(() => {
-        if (user){
-            server.get('student/details', {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
+        const accToken = getAccessToken() ;
+        let source = axios.CancelToken.source() ;
+
+        if (!accToken){
+            console.log('refreshing accesstoken on null') ;
+            refreshAccessToken(source).catch(err => {
+                if (err.repsonse.data?.error.default === 'jwt expired'){
+                    console.log('logout called') ;
+                    logout() ;
                 }
-            }).then(result => {
-                const u = result.data.data ;
-                if (u){
-                    console.log('update state')
-                    setUserData({
-                        name: u.name,
-                        email: u.email,
-                        contact: u.contact,
-                        address: u.address 
-                    }) ;
-                    setUpdated(u.updated) ;
-                }
-            })
-            .catch((err) => {
-                console.error(err) ;
-                if (err.response?.data.error === 'jwt expired'){
-                    refreshAccessToken() ;
-                }
-            })
+            }) ;
         }
-    }, [user]);
+
+        
+        let interval = setInterval(() => {
+            console.log('refreshing access token every 10min')
+            let newS = axios.CancelToken.source() ;
+            refreshAccessToken(newS).catch(err => {
+                console.error(err) ;
+                if (err?.repsonse?.data?.error.default === 'jwt expired'){
+                    console.log('logout called') ;
+                    logout() ;
+                }
+            }) ;
+        }, 10 * 60 * 1000) ;
+
+
+        return () => {
+            clearInterval(interval) ;
+            source.cancel() ;
+            detailsSource.cancel() ;
+        }
+    }, []) ;
+
     return (
-        <div className="w-100 h-100 d-flex flex-column">
-            <Navbar />
-            <div className="container d-flex justify-content-center align-items-center flex-grow-1 flex-shrink-1">
-                <div className="card" style={{
-                    width: '25rem'
-                }}>
-                    <div className="card-header">
-                        Details
-                    </div>
-                    <div className="card-body">
-                        <form>
-                            <div className="mb-2">
-                                <label htmlFor="fn" className='form-label'>
-                                    Full Name
-                                </label>
-                                <input type='text' id="fn" className='form-control' required name='name' onChange={change} />
+        <>
+            <div className="w-100 h-100 d-flex flex-column">
+                <Navbar />
+
+                <div className="container d-flex justify-content-center align-items-center flex-grow-1 flex-shrink-1">
+                    {
+                        show ?
+                        <button onClick={loadDetails} className="btn btn-primary">Fetch Details</button>
+                        :
+                        <div className="card" style={{ width: '25rem' }}>
+                            <div className="card-header">
+                                Student Details
                             </div>
-                            <div className="mb-2">
-                                <label htmlFor="email" className='form-label'>
-                                    Email
-                                </label>
-                                <input type='email' id="email" className='form-control' name='email' required onChange={change}/>
+                            <div className="card-body">
+                                <form>
+                                    <div className="mb-2">
+                                        <label htmlFor="fn" className='form-label'>
+                                            Full Name
+                                        </label>
+                                        <input type='text' id="fn" className='form-control' defaultValue={data.fullName} required name='fullName' disabled={success} onChange={inputChange}  />
+                                        {
+                                            formErrors.fullName && <div className="mt-1 text-danger">{ formErrors.fullName }</div>
+                                        }
+                                    </div>
+                                    <div className="mb-2">
+                                        <label htmlFor="email" className='form-label'>
+                                            Email
+                                        </label>
+                                        <input type='email' id="email1" className='form-control' disabled={success} name='email' defaultValue={data.email} required onChange={inputChange} />
+                                        {
+                                            formErrors.email && <div className="mt-1 text-danger">{ formErrors.email }</div>
+                                        }
+                                    </div>
+                                    <div className="mb-2">
+                                        <label htmlFor="con" className='form-label'>
+                                            Contact Number
+                                        </label>
+                                        <input type='tel' id="con" className='form-control' disabled={success} name='contact' defaultValue={data.contact} required onChange={inputChange} />
+                                        {
+                                            formErrors.contact && <div className="mt-1 text-danger">{ formErrors.contact }</div>
+                                        }
+                                    </div>
+                                    <div className="mb-2">
+                                        <label htmlFor="add" className='form-label'>
+                                            Address
+                                        </label>
+                                        <textarea id="add" className='form-control' disabled={success} name='address' defaultValue={data.address}  required onChange={inputChange} />
+                                        {
+                                            formErrors.address && <div className="mt-1 text-danger">{ formErrors.address }</div>
+                                        }
+                                    </div>
+                                    <div className="mb-2">
+                                        <label htmlFor="pdf" className='form-label'>
+                                            File Upload
+                                        </label>
+                                        <input ref={fileRef} type='file' id="pdf" className='form-control' disabled={success} name='file' accept="application/pdf" required/>
+                                        {
+                                            formErrors.file && <div className="mt-1 text-danger">{ formErrors.file }</div>
+                                        }
+                                    </div>
+                                </form>
+                                {
+                                    formErrors.default && <p className="text-danger text-center">{ formErrors.default }</p>
+                                }
                             </div>
-                            <div className="mb-2">
-                                <label htmlFor="con" className='form-label'>
-                                    Contact Number
-                                </label>
-                                <input type='tel' id="con" className='form-control' name='contact' required onChange={change}/>
+                            <div className="card-footer d-flex justify-content-end">
+                                <button className="btn btn-primary" disabled={busy ^ success} onClick={submit}>Update</button>
                             </div>
-                            <div className="mb-2">
-                                <label htmlFor="add" className='form-label'>
-                                    Address
-                                </label>
-                                <textarea id="add" className='form-control' name='address' required onChange={change}/>
-                            </div>
-                            <div className="mb-2">
-                                <label htmlFor="pdf" className='form-label'>
-                                    File Upload
-                                </label>
-                                <input type='file' id="pdf" className='form-control' name='file' required/>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="card-footer d-flex justify-content-end">
-                        <button className='btn btn-primary' disabled={updated} onClick={submit}>
-                            Update
-                        </button>
-                    </div>
+                        </div>
+                    }
                 </div>
             </div>
-        </div>
+        </>
     )
 } ;
 
