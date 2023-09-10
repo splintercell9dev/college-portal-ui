20d2a03955eae06b73a99633807e1c41057bf0eb
splintercell9dev <splintercell9dev@gmail.com>
2023-09-10 13:43:23 +0530

    Changed login component to have only one button for login.

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,146 +1,182 @@
-import { useState } from 'react';
-import server from '../../utils/axios' ;
+import { useState, useRef } from 'react';
+import server from '../../utils/axios';
 import './index.css' ;
-import { useNavigate } from 'react-router-dom';
-import { setUser } from '../../store/user'; 
+import { decodedToken, getRefreshToken, setAccessToken, setRefreshToken } from '../../utils/functions';
 import { useDispatch } from 'react-redux';
+import { setUser } from '../../store/user';
+import { useNavigate } from 'react-router-dom';
 
 function Login(){
-    const navigate = useNavigate() ;
+    const [login, setLogin] = useState(true) ;
     const dispatch = useDispatch() ;
-    const [signUp, setSignUp] = useState(false) ;
-    const [form, setForm] = useState({
-        username: '',
-        password: ''
+    const navigate = useNavigate() ;
+    const formRef = useRef() ;
+    const [formErrors, setFormErrors] = useState({
+        name: '',
+        password: '',
+        default: '',
+        repass: '',
+        role: ''
     }) ;
 
-    const [formValid, setValid] = useState(true) ;
+    const [formValid, setFormValid] = useState(true) ;
+    const [busy, setBusy] = useState(false) ;
+    const toggleLogin = () => setLogin(!login) ;
 
-    const [formErrors, setFormErros] = useState({
-        username: false,
-        password: false,
-    }) ;
 
-    const [serverError, setServerError] = useState('') ;
-
-    const toggle = () => setSignUp(!signUp) ;
-
-    const change = (ev) => {
-        const {name, value} = ev.target ;
-        setForm({
-            ...form,
-            [name]: value
-        }) ;
-        // validate(name, value) ;
-    }
-
-    const validate = (name, value) => {
-        if (name === 'username' && value && value.length >= 5 && value.length <= 20){
-            setFormErros({
-                ...formErrors,
-                username: false 
-            }) ;
+    const pwdCheck = (ev) => {
+        if (ev.target.value !== formRef.current.password.value){
+            console.log('not same') ;
+            setFormValid(false) ;
+            setFormErrors(e => ({
+                ...e,
+                repass: 'Password must be same.'
+            })) ;
         }
         else{
-            setFormErros({
-                ...formErrors,
-                username: true 
-            }) ;
-        }
-
-        if (name === 'password' && value){
-            if (value.length <= 8 && value.length >= 16){
-                setFormErros({
-                    ...formErrors,
-                    password: true 
-                }) ;
-            }
+            setFormValid(true) ;
+            setFormErrors(e => ({
+                ...e,
+                repass: ''
+            })) ;
         }
     } ;
 
-    const submit = (staff = false) => {
-        console.log('submit started')
-        if (!formErrors.username && !formErrors.password){
-            console.log('no form errors')
-            server.post(signUp ? 'signup' : 'login', {
-                ...form,
-                role: staff ? 'STAFF' : 'STUDENT'
-            }).then( result => {
-                console.log(result) ;
-                dispatch(setUser(result.data.data)) ;
-                navigate(staff ? 'staff' : 'student', {
-                    replace: true
-                })
-            })
-            .catch(err => {
-                console.log(err) ;
-                setServerError(err.response?.data.error || err.message) ;
+    const submit = () => {
+        console.log('inside submit')
+        if (formValid){
+            console.log('inside call');
+            setBusy(true) ;
+            
+            server.post(login ? 'login': 'signup', {
+                username: formRef.current.username.value,
+                password: formRef.current.password.value,
+                role: formRef.current.role.value
+            }).then( resp => {
+                const result = resp.data.data ;
+                if (result){
+                    setAccessToken(result.accessToken) ;
+                    setRefreshToken(result.refreshToken) ;
+                    const payload = {
+                        details: decodedToken(),
+                        refreshToken: getRefreshToken()
+                    } ;
+                    dispatch(setUser(payload));
+
+                    navigate(payload.details.role === 'STUDENT' ? 'student':'staff', { replace: true }) ;
+                }
+                setBusy(false) ;
+                setFormErrors(e => ({
+                    name: '',
+                    password: '',
+                    repass: '',
+                    default: '',
+                    role: ''
+                })) ;
+            }).catch(err => {
+                console.error(err);
+                setBusy(false) ;
+                const error = err.response?.data?.error ;
+                if (error && error.default){
+                    setFormErrors(e => ({
+                        ...e,
+                        default: error.default
+                    })) ;
+                }
+                else if (error){
+                    console.log('set form errors', error) ;
+                    setFormErrors(e => ({
+                        ...e,
+                        name: error.username || '',
+                        password: error.password || '',
+                        role: error.role || ''
+                    })) ;
+                }
+
             })
         }
     }
 
-
     return (
-        <div id="login-container" className="container">
+        <div id="login-container" className="container h-100 d-flex justify-content-center align-items-center">
             <div className="card" id="login-card" style={{
                 width: '22rem'
             }}>
                 <div className="card-header text-center">
                     {
-                        signUp ? 'SignUp' : 'Login'
+                        login ? 'Login' : 'SignUp'
                     }
                 </div>
                 <div className="card-body">
-                    <form>
+                    <form ref={formRef}>
                         <div className="mb-2">
                             <label htmlFor="user1" className='form-label'>
                                 Username
                             </label>
-                            <input type='text' id="user1" className='form-control' required name='username' onChange={change} />
+                            <input type='text' id="user1" className='form-control' required name='username' />
+                            {
+                                formErrors.name && <div className='text-danger mt-1'>{ formErrors.name }</div>
+                            }
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="role" className='form-label'>
+                                Role
+                            </label>
+                            <select type='text' id="role" className='form-control form-select' required name='role' defaultValue="STUDENT">
+                                <option value="STUDENT">Student</option>
+                                <option value="STAFF">Staff</option>
+                            </select>
+                            {
+                                formErrors.role && 
+                                <div className='text-danger mt-1'>{ formErrors.role }</div>
+                            }
                         </div>
                         <div className="mb-2">
                             <label htmlFor="pass1" className='form-label'>
                                 Password
                             </label>
-                            <input type='password' id="pass2" className='form-control' name='password' required onChange={change}/>
+                            <input type='password' id="pass1" className='form-control' name='password' required />
+                            {
+                                formErrors.password && 
+                                <div className='text-danger mt-1'>{ formErrors.password }</div>
+                            }
                         </div>
                     </form>
                     {
-                        signUp ?
+                        !login ?
                         <>
                             <div className="mb-2">
                                 <label htmlFor="pass2" className='form-label'>
                                     Re-Enter Password
                                 </label>
-                                <input type='password' id="pass2" className='form-control' name='recheck' required onChange={change} />
+                                <input type='password' id="pass2" className='form-control' name='recheck' required onChange={pwdCheck} />
+                                {
+                                    formErrors.repass && <div className="text-danger mt-1">{ formErrors.repass }</div>
+                                }
                             </div>
                             <p id="signup-link" className='py-2 mb-0' style={{
                                 fontSize: '12px'
                             }}>
-                                Already a User ? SignIn <a className='text-decoration-none' onClick={toggle}>here</a>
+                                Already a User ? SignIn <a className='text-decoration-none' onClick={toggleLogin}>here</a>
                             </p>
                         </>
                         :
                         <p id="signup-link" className='py-2 mb-0' style={{
                             fontSize: '12px'
                         }}>
-                            Not a User ? SignUp <a className='text-decoration-none' onClick={toggle}>here</a>
+                            Not a User ? SignUp <a className='text-decoration-none' onClick={toggleLogin}>here</a>
                         </p>
                     }
 
                     {
-                        serverError ?
-                        <p className='text-danger'>{ serverError }</p>
-                        :
-                        null
+                        formErrors.default && <p className='text-danger'>{ formErrors.default }</p>
                     }
                 </div>
-                <div className="card-footer d-flex flex-wrap justify-content-center p-3">
-                    <button className='btn btn-primary me-3' onClick={() => submit()} disabled={!formValid}>
-                        Student
-                    </button>
-                    <button className='btn btn-secondary' onClick={() => submit(true)} disabled={!formValid}>
-                        Staff
+                <div className="card-footer d-flex justify-content-end p-3">
+                    <button className='btn btn-primary' onClick={submit} disabled={formValid && busy}>
+                        {
+                            login ? 'Login': 'SignUp'
+                        }
                     </button>
                 </div>
             </div>
